bbcce1cbc40cf66e874650cb165181ba616aa301
ablake <student@ablake.local>
2017-01-09 10:43:50 -0600

    Initial Commit

---
+++ ticTacToe/GridLabel.swift
@@ -0,0 +1,16 @@
+//
+//  GridLabel.swift
+//  ticTacToe
+//
+//  Created by ablake on 11/18/16.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class GridLabel: UILabel
+{
+    var xTurn = true // Checks the turn of X or O
+    var canTap = true // Checks if label is tappable
+    var count = 0
+}
